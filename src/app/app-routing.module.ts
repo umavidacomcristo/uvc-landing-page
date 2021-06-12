ccc6715125969ebf447ee13e7f451894cdbd0d01
Matheus Phelipe <matheusphalves@gmail.com>
2021-06-12 20:43:07 -0300

    added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/shared/home/home.component';
+import { SplashComponent } from './components/shared/splash/splash.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+
+  { path: 'splash', component: SplashComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
