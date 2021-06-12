0308139871a9d341ad646bba0867b51a0c1af79b
Matheus Phelipe <matheusphalves@gmail.com>
2021-06-12 20:42:55 -0300

    injected deviceDetector service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NbMenuItem, NbMenuService } from '@nebular/theme';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'uvc-landing-page';
 
 
@@ -22,5 +24,18 @@ export class AppComponent {
       link: 'dashboard'
     }
    ];
+
+
+   constructor(
+     private deviceDetector: DeviceDetectorService,
+     private router: Router){}
+  ngOnInit(): void {
+    if(this.deviceDetector.isDesktop()){
+      this.router.navigate(['/']);
+    }else{
+      this.router.navigate(['/splash'])
+    }
+  }
+
 
 }
